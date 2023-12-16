8a38b1e748e27c5028138d114fb1a1b1dd3c2326
Binye Barwe <binyebarwe@gmail.com>
2023-12-16 12:51:42 -0500

    filter total amoutns by user

--- getTotalAmounts.ts
+++ getTotalAmounts.ts
@@ -20,40 +20,47 @@ async function main() {
   const marketPubkey = new PublicKey(
     "C3YPL3kYCSYKsmHcHrPWx1632GUXGqi2yMXJbfeCc57q"
   );
-  const ooa = new PublicKey("EaaK2PxkpA7mmtpWYaPYJWPBZ6pnYeiEQpywWcibWjW4");
+  const owner = new PublicKey("EaaK2PxkpA7mmtpWYaPYJWPBZ6pnYeiEQpywWcibWjW4");
 
   const market = await client.getMarketAccount(marketPubkey);
 
   if (market === null) {
     throw "No market";
   }
-  const openorders = await client.findOpenOrdersForMarket(ooa, marketPubkey);
-  const openOrder = await client.getOpenOrders(openorders[0]);
-
-  if (openOrder) {
-    console.log("bidsBaseLots", openOrder.position.bidsBaseLots.toNumber());
-    console.log("asksBaseLots", openOrder.position.asksBaseLots.toNumber());
-  }
-  const booksideBids = await client.getBookSide(market.bids);
-  if (booksideBids === null) {
-    throw "No booksideBids";
-  }
-  const bids = await client.getLeafNodes(booksideBids);
-
+  const openorders = await client.findOpenOrdersForMarket(owner, marketPubkey);
   let totalQuoteBids = 0;
-  for (const bid of bids) {
-    console.log(
-      "price: ",
-      priceData(bid.key),
-      "bid.quantity:",
-      bid.quantity.toNumber(),
-      "priceData(bid.key) * bid.quantity.toNumber():",
-      priceData(bid.key) * bid.quantity.toNumber()
-    );
-    totalQuoteBids +=
-      priceData(bid.key) *
-      bid.quantity.toNumber() *
-      market.quoteLotSize.toNumber();
+
+  for (const openOrderPubkey of openorders) {
+    const openOrder = await client.getOpenOrders(openOrderPubkey);
+
+    if (openOrder) {
+      console.log("bidsBaseLots", openOrder.position.bidsBaseLots.toNumber());
+      console.log("asksBaseLots", openOrder.position.asksBaseLots.toNumber());
+    }
+    const booksideBids = await client.getBookSide(market.bids);
+    if (booksideBids === null) {
+      throw "No booksideBids";
+    }
+    const bids = await client.getLeafNodes(booksideBids);
+
+    for (const bid of bids) {
+      if (openOrderPubkey.toString() !== bid.owner.toString()) {
+        continue;
+      }
+      console.log(bid);
+      console.log(
+        "price: ",
+        priceData(bid.key),
+        "bid.quantity:",
+        bid.quantity.toNumber(),
+        "priceData(bid.key) * bid.quantity.toNumber():",
+        priceData(bid.key) * bid.quantity.toNumber()
+      );
+      totalQuoteBids +=
+        priceData(bid.key) *
+        bid.quantity.toNumber() *
+        market.quoteLotSize.toNumber();
+    }
   }
 
   console.log("totalQuoteBidsNative", totalQuoteBids);
